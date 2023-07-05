6007d6686dc393c7adfdecd1deeef141ede1f60f
Stan Zaytsev <stan.zaytsev@onsend.com>
2023-07-05 22:39:10 +1200

    Install @gmap-vue/v3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+import { GmapVuePlugin } from '@gmap-vue/v3';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(GmapVuePlugin, {
+  load: {
+    key: import.meta.env.VITE_GOOGLE_MAPS_KEY
+  }
+})
+app.mount('#app')
