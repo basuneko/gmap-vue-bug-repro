3e16368c40f82dfb24c1b4e308bc729bb63e8379
Stan Zaytsev <stan.zaytsev@onsend.com>
2023-07-07 13:38:10 +1200

    Bug: marker cannot be removed with v-if

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <GmvMap :center="{ lat: 10, lng: 10 }" :zoom="7" map-type-id="terrain">
-    <GmvMarker :position="{ lat: 10, lng: 10 }" />
-  </GmvMap>
+  <div>
+    <div class="buttons">
+      <button @click="showMarker = !showMarker">Toggle Marker</button>
+    </div>
+    <div class="map">
+      <GmvMap :center="{ lat: 10, lng: 10 }" :zoom="7" map-type-id="terrain">
+        <GmvMarker v-if="showMarker" :position="{ lat: 10, lng: 10 }" />
+      </GmvMap>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+const showMarker = ref(false)
 </script>
 
 <style>
+.buttons {
+  padding: 10px;
+  display: flex;
+  gap: 20px;
+}
+
 .gmv-map {
   width: 100vw;
   height: 100vh;
